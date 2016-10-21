58751435f44ee5a65658e966906eb999f9e06901
Emily Eng <emilyeng@me.com>
2016-10-20 21:23:57 -0400

    make google calendar api work

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 (function() {
-
+    var slackkey = 'xoxp-3569420125-10493169398-94086187185-603e68a8532db55788b633fafd9983a1';
     var STATUS_URL = "https://slack.com/api/users.getPresence?token="+ slackkey +"&user=";
 
     var IMAGE_CONTAINER_SELECTOR = ".imageContainer",
         SLACK_CONTAINER_SELECTOR = ".slackContainer",
         GUIDE_UL_SELECTOR = ".guide-ul",
+        DISPLAY_NONE_CLASS = "display--none",
+        DISPLAY_INLINE_CLASS = "display--inline",
+        GCAL_ACCEPTED_STATUS = "accepted",
+        CUR_CAL_NUM = 0,
         BISHOP_ID = "Bishop",
         CARLOUGH_ID = "Carlough",
         HOWARD_ID = "Howard",
@@ -16,32 +20,39 @@
         RUIZ_SLACK = "U0AHRPE3B",
         WEAVER_SLACK = "U0K5JA464";
 
-    var SLACK_LIST = [{"name": BISHOP_ID, "user": BISHOP_SLACK, "color": "#d1f9ff"},
-                    {"name": CARLOUGH_ID, "user": CARLOUGH_SLACK, "color": "#feff86"},
-                    {"name": HOWARD_ID, "user": HOWARD_SLACK, "color": "#e8a6ff"},
-                    {"name": RUIZ_ID, "user": RUIZ_SLACK, "color": "#ff9797"},
-                    {"name": WEAVER_ID, "user": WEAVER_SLACK, "color": "#9aff91"}],
+    var WILL_LIST = [{"name": BISHOP_ID, "user": BISHOP_SLACK, "color": "#d1f9ff", "email": "wbishop@meetup.com"},
+                    {"name": CARLOUGH_ID, "user": CARLOUGH_SLACK, "color": "#feff86", "email": "wcarlough@meetup.com"},
+                    {"name": HOWARD_ID, "user": HOWARD_SLACK, "color": "#e8a6ff", "email": "whoward@meetup.com"},
+                    {"name": RUIZ_ID, "user": RUIZ_SLACK, "color": "#ff9797", "email": "willr@meetup.com"},
+                    {"name": WEAVER_ID, "user": WEAVER_SLACK, "color": "#9aff91", "email": "wweaver@meetup.com"}],
         COORDINATE_MAP = {
             "Bishop": {"x": "27%", "y": "31%"}, //18
             "Carlough": {"x": "76%", "y": "82%"}, //95
             "Howard": {"x": "55%", "y": "40%"}, //42 or 40
             "Ruiz": {"x": "20%", "y": "90%"}, //14
             "Weaver": {"x": "44%", "y": "94%"}, //48
-            "10front": {"x": "14%", "y": "66%"},
-            "11back": {"x": "0%", "y": "0%"},
-            "11mid": {"x": "10%", "y": "50%"},
+            "10th Floor Meeting Room": {"x": "14%", "y": "66%"},
+            "Sushi Dog": {"x": "0%", "y": "0%"},
+            "Reading Room": {"x": "0%", "y": "0%"},
+            "Pride": {"x": "0%", "y": "0%"},
+            "11th - Back": {"x": "0%", "y": "0%"},
+            "11th - Middle": {"x": "10%", "y": "50%"},
+            "Poodle": {"x": "0%", "y": "0%"},
+            "BobRoss": {"x": "0%", "y": "0%"},
+            "Pairing Station": {"x": "0%", "y": "0%"},
         },
         CALENDAR_MAP = {
-            "10front": "meetup.com_33383337313533302d373236@resource.calendar.google.com",
-            "sushidog": "meetup.com_363534363636383838@resource.calendar.google.com",
-            "readingroom": "meetup.com_33303231363532343633@resource.calendar.google.com",
-            "pride": "meetup.com_2d3930303636323736323837@resource.calendar.google.com",
-            "11back": "meetup.com_2d363231343931382d323732@resource.calendar.google.com",
-            "11mid": "meetup.com_31383438323937312d383133@resource.calendar.google.com",
-            "poodle": "meetup.com_2d3830303134353936373838@resource.calendar.google.com",
-            "bobross": "meetup.com_3334353237323633313737@resource.calendar.google.com",
-            "pairingstation": "meetup.com_2d37383634383431322d353730@resource.calendar.google.com",
-        };
+            "10th Floor Meeting Room": "meetup.com_33383337313533302d373236@resource.calendar.google.com",
+            "Sushi Dog": "meetup.com_363534363636383838@resource.calendar.google.com",
+            "Reading Room": "meetup.com_33303231363532343633@resource.calendar.google.com",
+            "Pride": "meetup.com_2d3930303636323736323837@resource.calendar.google.com",
+            "11th - Back": "meetup.com_2d363231343931382d323732@resource.calendar.google.com",
+            "11th - Middle": "meetup.com_31383438323937312d383133@resource.calendar.google.com",
+            "Poodle": "meetup.com_2d3830303134353936373838@resource.calendar.google.com",
+            "BobRoss": "meetup.com_3334353237323633313737@resource.calendar.google.com",
+            "Pairing Station": "meetup.com_2d37383634383431322d353730@resource.calendar.google.com",
+        },
+        GCAL_CURRENT_EVENTS_LIST = []; // name, room, Bishop, Carlough, Howard, Ruiz, Weaver 
         
     
 
@@ -59,9 +70,7 @@
         $personForGuide.addClass("display--inlineBlock");
         $li.append($personForGuide);
         $li.append("<div class='display--inlineBlock'>Will " + currentWill.name + "</div>");
-        $(GUIDE_UL_SELECTOR).append($li); 
-        
-        checkSlackStatus(currentWill);
+        $(GUIDE_UL_SELECTOR).append($li);
     }
 
     function checkSlackStatus(currentWill) {
@@ -70,40 +79,233 @@
         $.ajax({
                 url: willStatusUrl,
                 dataType: "json",
-                success:
-                    function(data) {
-                        var name = currentWill.name,
-                            status = data.presence,
-                            autoAwayStatus = data.auto_away;
+                success: function(data) {
+                    var name = currentWill.name,
+                        status = data.presence,
+                        autoAwayStatus = data.auto_away;
 
-                        switch(status) {
-                            case "active":
-                                $(SLACK_CONTAINER_SELECTOR).append("<div>Will " + name + " is active.</div>");
-                                break;
-                            case "away":
-                                if (autoAwayStatus) {
-                                    $(SLACK_CONTAINER_SELECTOR).append("<div>Will " + name + " is gone.</div>");
-                                } else {
-                                    $(SLACK_CONTAINER_SELECTOR).append("<div>Will " + name + " is away on purpose.</div>");
-                                }
-                                break;
+                    switch(status) {
+                        case "active":
+                            //$(SLACK_CONTAINER_SELECTOR).append("<div>Will " + name + " is active.</div>");
+                            console.log("Will " + name + " is active.");
+                            break;
+                        case "away":
+                            if (autoAwayStatus) {
+                                //$(SLACK_CONTAINER_SELECTOR).append("<div>Will " + name + " is gone.</div>");
+                                console.log("Will " + name + " is gone.");
+                            } else {
+                                //$(SLACK_CONTAINER_SELECTOR).append("<div>Will " + name + " is away on purpose.</div>");
+                                console.log("Will " + name + " is away on purpose.");
+                            }
+                            break;
+                    }
+
+                    var willHasGcalEvent = false,
+                        gcalEvent = null;
+                    for (var ev in GCAL_CURRENT_EVENTS_LIST) {
+                        if (ev[currentWill] === true) {
+                            willHasGcalEvent = true;
+                            gcalEvent = ev;
                         }
+                    }
 
+                    // decide whether to use slack or gcal location
+                    if (willHasGcalEvent && gcalEvent !== null) {
+                        $("#" + name).css("left", COORDINATE_MAP[gcalEvent.room].x);
+                        $("#" + name).css("top", COORDINATE_MAP[gcalEvent.room].y);
+                    } else {
                         $("#" + name).css("left", COORDINATE_MAP[name].x);
                         $("#" + name).css("top", COORDINATE_MAP[name].y);
+                    }
                 }
         });
     }
 
-    function checkGoogleCalendar() {
-        // TODO
-    }
-
-    function slackPick() {
-        for (var i = 0; i <SLACK_LIST.length; i++){
-            createPerson(SLACK_LIST[i]);
+    function createPeople() {
+        for (var i = 0; i <WILL_LIST.length; i++){
+            createPerson(WILL_LIST[i]);
         }
     }
 
-    $(document).ready(slackPick);
+    // Your Client ID can be retrieved from your project in the Google
+    // Developer Console, https://console.developers.google.com
+    var CLIENT_ID = "437324888638-kjas2gbg9qo7oivhi1bkhl5i64cbpsj8.apps.googleusercontent.com";
+
+    var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+    /**
+     * Check if current user has authorized this application.
+     */
+    function checkAuth() {
+        gapi.auth.authorize({
+            'client_id': CLIENT_ID,
+            'scope': SCOPES,
+            'immediate': true
+        }, handleAuthResult);
+    }
+
+    /**
+     * Initiate auth flow in response to user clicking authorize button.
+     *
+     * @param {Event} e Button click event.
+     */
+    function handleAuthClick(e) {
+        gapi.auth.authorize({
+            client_id: CLIENT_ID,
+            scope: SCOPES,
+            immediate: false
+        }, handleAuthResult);
+        return false;
+    }
+
+    /**
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
+    function handleAuthResult(authResult) {
+        var $authorizeDiv = $('#authorize-div');
+        if (authResult && !authResult.error) {
+            // Hide auth UI, then load client library.
+            $authorizeDiv.addClass(DISPLAY_NONE_CLASS);
+            $authorizeDiv.removeClass(DISPLAY_INLINE_CLASS);
+
+            loadCalendarApi();
+        } else {
+            // Show auth UI, allowing the user to initiate authorization by
+            // clicking authorize button.
+            $authorizeDiv.addClass(DISPLAY_INLINE_CLASS);
+            $authorizeDiv.removeClass(DISPLAY_NONE_CLASS);
+        }
+    }
+
+    /**
+     * Load Google Calendar client library. List upcoming events
+     * once client library is loaded.
+     */
+    function loadCalendarApi() {
+        gapi.client.load('calendar', 'v3', populateGcalList);
+    }
+
+    /**
+     * Print the summary and start datetime/date of the next ten events in
+     * the authorized user's calendar. If no events are found an
+     * appropriate message is printed.
+     */
+    function populateGcalList() {
+        for (var calendar in CALENDAR_MAP) {
+            CUR_CAL_NUM++;
+            var nowMinusTwoHours = new Date(),
+                currentHours = nowMinusTwoHours.getHours();
+            nowMinusTwoHours.setHours(currentHours - 4);
+            var calendarId = CALENDAR_MAP[calendar],
+                request = gapi.client.calendar.events.list({
+                    'calendarId': calendarId,
+                    'timeMin': nowMinusTwoHours.toISOString(),
+                    'showDeleted': false,
+                    'singleEvents': true,
+                    'maxResults': 10,
+                    'orderBy': 'startTime'
+                });
+            request.execute(handleEventListResponse);
+        }
+    }
+
+    function handleEventListResponse(resp) {
+        var events = resp.items;
+
+        if (events.length > 0) {
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i],
+                    startTime = event.start.dateTime,
+                    endTime = event.end.dateTime,
+                    start = new Date(startTime),
+                    end = new Date(endTime),
+                    isHappeningNow = isEventHappeningNow(startTime, endTime);
+
+                var date = (start.getMonth() + 1) + '/' + start.getDate() + '/' + start.getFullYear(),
+                    timeRange = start.getHours() + ':' + start.getMinutes() + ' - ' + end.getHours() + ':' + end.getMinutes(),
+                    happening = isHappeningNow ? 'happening now' : 'NOT happening now';
+
+                console.log(event.summary + ' (' + date + ' @ ' + timeRange + ', ' + happening + ')');
+                
+                if (isHappeningNow && event.attendees !== undefined) {
+                    var name = event.summary,
+                        room = event.location,
+                        bAttending = false,
+                        cAttending = false,
+                        hAttending = false,
+                        rAttending = false,
+                        wAttending = false;
+                    for (var will in WILL_LIST) {
+                        for (var a in event.attendees) {
+                            if (a.email == will.email && a.responseStatus == GCAL_ACCEPTED_STATUS) {
+                                switch(will.name) {
+                                    case BISHOP_ID:
+                                        bAttending = true;
+                                        break;
+                                    case CARLOUGH_ID:
+                                        cAttending = true;
+                                        break;
+                                    case HOWARD_ID:
+                                        hAttending = true;
+                                        break;
+                                    case RUIZ_ID:
+                                        rAttending = true;
+                                        break;
+                                    case WEAVER_ID:
+                                        wAttending = true;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    GCAL_CURRENT_EVENTS_LIST.push({
+                        "name": name,
+                        "room": room,
+                        "Bishop": bAttending,
+                        "Carlough": cAttending,
+                        "Howard": hAttending,
+                        "Ruiz": rAttending,
+                        "Weaver": wAttending
+                    });
+                }
+            }
+            console.log(GCAL_CURRENT_EVENTS_LIST);
+        }
+        if (CUR_CAL_NUM == 9) {
+            for(var j = 0; j <WILL_LIST.length; j++) {
+                checkSlackStatus(WILL_LIST[j]);
+            }
+        }
+    }
+
+    function isEventHappeningNow(startTime, endTime) {
+        var now = new Date(),
+            start = new Date(startTime),
+            end = new Date(endTime);
+        return now <= end && now >= start;
+    }
+
+    /**
+    * Append an element to the output div containing the given message.
+    *
+    * @param {string} message Text to be placed in output element.
+    */
+    function appendToOutput(message) {
+        var $output = $("#output");
+        $output.append("<div>" + message + "\n");
+    }
+
+    function bindEvents() {
+        $("#authorize-button").on("click", handleAuthClick);
+    }
+
+    function init() {
+        bindEvents();
+        createPeople();
+    }
+
+
+    $(document).ready(init);
 })();
